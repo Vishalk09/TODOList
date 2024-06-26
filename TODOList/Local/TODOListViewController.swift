8b8188ea5d76e7c49d5a2fdf82486308a868b377
Vishalk09 <vishalpkhokad0909@gmail.com>
2024-06-26 19:48:22 +0530

    Added delete option by swiping the cell

--- TODOList/Local/TODOListViewController.swift
+++ TODOList/Local/TODOListViewController.swift
@@ -12,12 +12,13 @@ class TODOListViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     let viewModel = TODOListViewModel()
     var todoData = [LocalTODOListModel]()
-    let adVCObj = AddDataViewController()
+    let addVCObj = AddDataViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        adVCObj.delegate = self
+        self.addVCObj.delegate = self
+        
         viewModel.fetchData() { data in
             self.todoData = data
             self.tableView.reloadData()
@@ -60,6 +61,47 @@ extension TODOListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("Did tap on \(indexPath.row)")
+        var model = todoData[indexPath.row]
+        let alertController = UIAlertController(title: model.isCompleted ? "Mark incomplete?" : "Mark completed?", message: nil, preferredStyle: .alert)
+        let alertActionOk = UIAlertAction(title: "Ok", style: .default) { _ in
+            model.isCompleted = !model.isCompleted
+            let viewModel = TODOListViewModel()
+            viewModel.updateData(model)
+            viewModel.fetchData() { data in
+                self.todoData = data
+                tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+        }
+        let alertActionCancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alertController.addAction(alertActionOk)
+        alertController.addAction(alertActionCancel)
+        
+        self.present(alertController, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete?") { [weak self] _,_,_ in
+            let alertController = UIAlertController(title: "Delete?", message: "Do you want to delete?", preferredStyle: .alert)
+            let alertAction1 = UIAlertAction(title: "Yes", style: .default) { _ in
+                self?.viewModel.deleteData("\(self?.todoData[indexPath.row].date ?? Date())")
+                self?.viewModel.fetchData() { data in
+                    self?.todoData = data
+                }
+                self?.tableView.reloadData()
+            }
+            let alertAction2 = UIAlertAction(title: "No", style: .destructive) { _ in
+                // Cancelled
+            }
+            alertController.addAction(alertAction1)
+            alertController.addAction(alertAction2)
+            
+            self?.present(alertController, animated: true)
+        }
+        deleteAction.backgroundColor = .red
+        
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+        return configuration
     }
 }
 
